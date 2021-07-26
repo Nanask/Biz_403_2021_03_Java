a5da32ac1831ecdc9952c2722e46178d38788d30
Nanask <ybhag1005@daum.net>
2021-07-26 17:45:04 +0900

    app

---
+++ Java_024_Lambda/src/com/callor/lam/service/impl/OnclickServiceImplV1.java
@@ -0,0 +1,15 @@
+package com.callor.lam.service.impl;
+
+import com.callor.lam.service.OnClickService;
+
+public class OnclickServiceImplV1 implements OnClickService{
+
+	@Override
+	public void onClick(String msg) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(msg);
+		
+	}
+
+}
